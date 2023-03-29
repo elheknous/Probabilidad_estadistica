9bdd96e0ef0e4e116ba7b08366efb736d5542e90
unknown <josematustoro6@gmail.com>
2023-03-29 14:05:48 -0300

    Tabla de frecuencia

--- tabla_frecuencias.R
+++ tabla_frecuencias.R
@@ -6,4 +6,15 @@ tipi <- transform(tablaFrecuencia,
           freAcumulada = cumsum(tablaFrecuencia$Freq),
           frecRelativa = round(prop.table(tablaFrecuencia$Freq),3),
           frecRelativaAcumulada = round(cumsum(prop.table(tablaFrecuencia$Freq)),3))
+tipi
+library(tidyverse)
+pokemon %>%
+  group_by(attack) %>% 
+  tally()
 
+base2 = pokemon %>% 
+  group_by(generation) %>% 
+  summarise(total = n(),
+            media_ataques= mean(attack),
+            ataques_min = min(attack))
+base2
